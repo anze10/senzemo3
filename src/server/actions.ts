289cc34caec222e93660ae7ae177cd8c3abdc7cc
Anže Repše <anze.repse@gmail.com>
2024-08-01 09:32:14 +0200

    neki

--- src/server/actions.ts
+++ src/server/actions.ts
@@ -14,13 +14,13 @@ export async function create_spreatsheet() {
 }
   const avth = new JWT({
     email: session?.user?.email,
-    key: env.NEXT_PUBLIC_API_KEY,
+    key: env.PRIVATE_KEY,
     scopes: [
       'https://www.googleapis.com/auth/spreadsheets',
       // note that sharing-related calls require the google drive scope
       'https://www.googleapis.com/auth/drive.file',
     ],
   });
-  const newDoc = GoogleSpreadsheet.createNewSpreadsheetDocument(avth, {title: 'test'}).then(console.log).catch(e => console.log('error', e));
+  const newDoc = await GoogleSpreadsheet.createNewSpreadsheetDocument(avth, {title: 'test'}).then(console.log).catch(e => console.log('error', e));
   console.log(newDoc)
 }
