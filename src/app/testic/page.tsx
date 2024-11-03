f1dcb9521422059bd541a90e696d2d59cd2042e5
Anze10 <anze.repse@gmail.com>
2024-11-03 11:08:20 +0100

    build

--- src/app/testic/page.tsx
+++ src/app/testic/page.tsx
@@ -1,11 +1,11 @@
 
-import PrinterListClient from '../dev/components/printer/PrinterList';
+
 
 export default async function Home() {
     return (
         <div>
             <h1>Printer Management</h1>
-            <PrinterListClient />
+            
         </div>
     );
 }
